6ae9ef56c3f2de0f400241be74812080dd383f4d
Phan Thanh Tu <tupt0101@gmail.com>
2022-01-27 14:23:40 +0700

    remove hardcode

--- src/app/application.go
+++ src/app/application.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tupt0101/student-auth-service/src/http"
 	"github.com/tupt0101/student-auth-service/src/repository/db"
@@ -18,5 +20,5 @@ func StartApplication() {
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
 	router.POST("/oauth/access_token", atHandler.Create)
 
-	router.Run(":9092")
+	router.Run(":" + os.Getenv("APP_PORT"))
 }
